c54045af7767a66b23ef4bc602a8e604aa5b01d5
A22Marti <a22marroibro@inspedralbes.cat>
2023-10-18 19:01:02 +0200

    newChanges

--- Ejercicio12/app/src/main/java/com/example/ejercicio12/MainActivity.kt
+++ Ejercicio12/app/src/main/java/com/example/ejercicio12/MainActivity.kt
@@ -1,22 +1,17 @@
-package com.example.ejercicio9
+package com.example.ejercicio12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CheckBox
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.view.View
 import android.widget.Toast
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-
-        }
-
+    }
+    fun onSubmitButtonClick(view: View) {
+        Toast.makeText(this, "Registro completado", Toast.LENGTH_SHORT).show()
     }
 }
